d10b76af562111d609a0c180b03ab0c9ad04a4ff
itssinghankit <singhankit.kr@gmail.com>
2023-10-17 18:28:59 +0530

    bcrypt and saltRounds

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 // const mongooseEncryption = require("mongoose-encryption")
-const md5=require("md5");
+// const md5=require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -15,7 +17,8 @@ mongoose.connect("mongodb://0.0.0.0/AuthDB", {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//////////////////////////////////user schema and model//////////////////
+//////////////////////////////////////////////////////////// user schema and model
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
@@ -37,36 +40,74 @@ app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/login.html");
 });
 
-////////////////////////////to save user on DB
+////////////////////////////////////////////////////////to save user on DB
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.email,
-        password: md5(req.body.password)
+
+    ////////////////////////////////////////////////////by using bcrypt and salting
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.email,
+            password: hash
+        });
+
+        newUser.save().then(() => {
+            res.sendFile(__dirname + "/userAccount.html");
+        })
+            .catch((err) => {
+                console.log(err);
+            });
+
     });
 
-    newUser.save().then(() => {
-        res.sendFile(__dirname + "/userAccount.html");
-    })
-        .catch((err) => {
-            console.log(err);
-        });
+    ////////////////////////////////////////////////////conventional md5 encryption
+
+    // const newUser = new User({
+    //     email: req.body.email,
+    //     password: md5(req.body.password)
+    // });
+
+    // newUser.save().then(() => {
+    //     res.sendFile(__dirname + "/userAccount.html");
+    // })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
 });
 
-//////////////////////////for login page
+//////////////////////////////////////////////////////////for login page
 
 app.post("/login", (req, res) => {
     const email = req.body.email;
-    const password = md5(req.body.password);
+    // const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: email })
         .then((foundUser) => {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.sendFile(__dirname + "/userAccount.html");
-                } else {
-                    res.send("wrong password");
-                }
+                //////////////////////////////////////////using bcrypt
+
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+
+                    if (result === true) {
+                        res.sendFile(__dirname + "/userAccount.html");
+                    } else {
+                        res.send("wrong password!");
+                    }
+
+
+                });
+
+                //////////////////////////////////////using previous methods of MD5 and mongoose encryption
+
+                // if (foundUser.password === password) {
+                //     res.sendFile(__dirname + "/userAccount.html");
+                // } else {
+                //     res.send("wrong password");
+                // }
             } else {
                 res.send("user not found");
             }
